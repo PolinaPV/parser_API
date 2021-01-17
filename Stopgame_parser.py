39f640fc11987843c12f3fd46fce43030fbd4fe5
Lycoris <podsipanina@gmail.com>
2021-01-17 06:10:19 +0300

    add README

--- Stopgame_parser.py
+++ Stopgame_parser.py
@@ -11,6 +11,7 @@ HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
 }
 
+
 logging.basicConfig(filename='parser_API.log', filemode='w', level=logging.DEBUG,
                     format='[%(levelname)s] => %(message)s')
 
